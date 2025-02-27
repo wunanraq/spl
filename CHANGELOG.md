053d44ea68e974152b75c8797146e66c15325d3f
wunanraq <wunan@raqsoft.com>
2025-02-27 17:01:11 +0800

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,3 @@ Check out the Metals release notes at [https://github.com/wunanraq/spl/](https:/
 
 ## VSCode Extension Changelog
 ### v1.0.1 (2025/02/25 09:28 +00:00)
-- [df1b306](https://github.com/wunanraq/spl/commit/df1b306ee2f4b03729331f40cee07c0e570d8013)
--- src/extension.ts
+++ src/extension.ts
@@ -100,7 +100,7 @@ async function executeSpl(filePath: string): Promise<string> {
       const productName = path.basename(esProcHome);
       const javaPath = `${startHome}/common/jre/bin/java`;
       const mainClass = "com.scudata.ide.spl.VSCodeApi";
-      const cmd = `"${javaPath}" -cp "${startHome}/esProc/lib/*;${startHome}/common/jdbc/*" -Dstart.home="${startHome}/${productName}" ${mainClass} -r "${filePath}"`;
+      const cmd = `"${javaPath}" -cp "${startHome}/${productName}/lib/*;${startHome}/common/jdbc/*;${startHome}/${productName}/classes" -Dstart.home="${startHome}/${productName}" ${mainClass} -r "${filePath}"`;
       // console.log(cmd);
       // 调用 Java 程序
       const process = exec(cmd, (error, stdout, stderr) => {
